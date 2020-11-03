58bb683dcb0bb5cc7a8ab1ef2e11384d78207c82
katebekk <85067@mail.ru>
2020-11-03 05:15:58 +0000

    dev test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,21 +7,29 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Dog;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Repository\DogRepository;
 
-
-/**
- * @Route("/index")
- */
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="index", methods={"POST","GET"})
+     * @Route("/index")
      */
-    public function ad(Request $request,DogRepository $dogRepository)
+    public function list()
     {
+        return new JsonResponse(
+            [
+                'env' => $_SERVER['APP_ENV'],
+                'list' => [
+                    random_int(0,100),
+                    random_int(0,100),
+                    random_int(0,100),
+                    random_int(0,100),
+                    random_int(0,100),
+                    random_int(0,100),
+                ]
+
+            ]
+        );
 
     }
 }
